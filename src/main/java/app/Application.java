0eaafeeb25934079aae69a98862319b7bc3729b2
belhassena.24 <budabelhassen@gmail.com>
2022-12-22 11:41:59 +0300

    render text

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,5 +1,6 @@
 package app;
 
+import controls.Label;
 import io.github.humbleui.jwm.*;
 import io.github.humbleui.jwm.skija.EventFrameSkija;
 import io.github.humbleui.skija.Canvas;
@@ -13,11 +14,20 @@ import java.io.File;
 import java.util.function.Consumer;
 
 import static app.Colors.APP_BACKGROUND_COLOR;
+import static app.Colors.PANEL_BACKGROUND_COLOR;
 
 /**
  * Класс окна приложения
  */
 public class Application implements Consumer<Event> {
+    /**
+     * Первый заголовок
+     */
+    private final Label label;
+    /**
+     * отступы панелей
+     */
+    public static final int PANEL_PADDING = 5;
     /**
  * радиус скругления элементов
  */
@@ -30,7 +40,9 @@ public static final int C_RAD_IN_PX = 4;
     /**
      * Конструктор окна приложения
      */
+
     public Application() {
+
         // создаём окно
         window = App.makeWindow();
         // задаём обработчиком событий текущий объект
@@ -62,7 +74,8 @@ public static final int C_RAD_IN_PX = 4;
             }
         }
 
-        // если окну не присвоен ни один из слоёв
+        // создаём первый заголовок
+        label = new Label(window, false, PANEL_BACKGROUND_COLOR, PANEL_PADDING, "Привет, мир!");
         if (window._layer == null)
             throw new RuntimeException("Нет доступных слоёв для создания");
         // делаем окно видимым
@@ -84,7 +97,7 @@ public static final int C_RAD_IN_PX = 4;
             window.close();
         } else if (e instanceof EventFrameSkija ee) {
             Surface s = ee.getSurface();
-            paint(s.getCanvas(), new CoordinateSystem2i(s.getWidth()/3, s.getHeight()/3, s.getWidth()/3, s.getHeight()/3));
+            paint(s.getCanvas(), new CoordinateSystem2i(s.getWidth(), s.getHeight()));
         }
     }
     /**
@@ -98,14 +111,10 @@ public static final int C_RAD_IN_PX = 4;
         canvas.save();
         // очищаем канвас
         canvas.clear(APP_BACKGROUND_COLOR);
-        // создаём кисть
-        Paint paint = new Paint();
-        // задаём цвет рисования
-        paint.setColor(Misc.getColor(100, 255, 255, 255));
-        // рисуем квадрат
-        canvas.drawRRect(windowCS.getRRect(4), paint);
         // восстанавливаем состояние канваса
         canvas.restore();
+        // рисуем заголовок в точке [100,100] с шириной и выостой 200
+        label.paint(canvas, new CoordinateSystem2i(100, 100, 200, 200));
     }
 }
 
