64ac73917487f8eb85e3d4bce04372ed54b35628
belhassena.24 <budabelhassen@gmail.com>
2023-01-30 10:42:49 +0300

    solve button

--- src/main/java/app/Task.java
+++ src/main/java/app/Task.java
@@ -58,12 +58,14 @@ public class Task {
      */
     public void clear() {
         points.clear();
+        solved = false;
     }
 
     /**
      * Решить задачу
      */
     public void solve() {
+        solved = true;
         PanelLog.warning("Вызван метод solve()\n Пока что решения нет");
     }
 
@@ -71,9 +73,20 @@ public class Task {
      * Отмена решения задачи
      */
     public void cancel() {
-
+        solved = false;
     }
-
+    /**
+     * проверка, решена ли задача
+     *
+     * @return флаг
+     */
+    public boolean isSolved() {
+        return solved;
+    }
+    /**
+     * Флаг, решена ли задача
+     */
+    private boolean solved;
     /**
      * Добавить точку
      *
@@ -81,6 +94,7 @@ public class Task {
      * @param pointSet множество
      */
     public void addPoint(Vector2d pos, Point.PointSet pointSet) {
+        solved = false;
         Point newPoint = new Point(pos, pointSet);
         points.add(newPoint);
         // Добавляем в лог запись информации
