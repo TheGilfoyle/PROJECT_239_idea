ee2e6398878ff2fe664a2d4d39d3811ad83887a3
belhassena.24 <budabelhassen@gmail.com>
2023-03-16 12:44:27 +0300

    info panel

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,6 +2,7 @@ package app;
 
 import controls.InputFactory;
 import controls.Label;
+import dialogs.PanelInfo;
 import io.github.humbleui.jwm.*;
 import io.github.humbleui.jwm.skija.EventFrameSkija;
 import io.github.humbleui.skija.Canvas;
@@ -18,8 +19,7 @@ import panels.PanelRendering;
 import java.io.File;
 import java.util.function.Consumer;
 
-import static app.Colors.APP_BACKGROUND_COLOR;
-import static app.Colors.PANEL_BACKGROUND_COLOR;
+import static app.Colors.*;
 
 /**
  * Класс окна приложения
@@ -62,9 +62,17 @@ public class Application implements Consumer<Event> {
      */
     private final Label label;
     /**
+     * Панель информации
+     */
+    private final PanelInfo panelInfo;
+    /**
      * отступы панелей
      */
     public static final int PANEL_PADDING = 5;
+    /**
+     * Текущий режим(по умолчанию рабочий)
+     */
+    public static Mode currentMode = Mode.WORK;
     /**
  * радиус скругления элементов
  */
@@ -73,6 +81,23 @@ public static final int C_RAD_IN_PX = 4;
      * окно приложения
      */
     private final Window window;
+    /**
+     * Режимы работы приложения
+     */
+    public enum Mode {
+        /**
+         * Основной режим работы
+         */
+        WORK,
+        /**
+         * Окно информации
+         */
+        INFO,
+        /**
+         * работа с файлами
+         */
+        FILE
+    }
 
     /**
      * Конструктор окна приложения
@@ -144,6 +169,8 @@ public static final int C_RAD_IN_PX = 4;
             throw new RuntimeException("Нет доступных слоёв для создания");
         // делаем окно видимым
         window.setVisible(true);
+        // панель информации
+        panelInfo = new PanelInfo(window, true, DIALOG_BACKGROUND_COLOR, PANEL_PADDING);
     }
 
     /**
@@ -175,11 +202,16 @@ public static final int C_RAD_IN_PX = 4;
                     // разбираем, какую именно кнопку нажали
                     switch (eventKey.getKey()) {
                         case ESCAPE -> {
-                            window.close();
-                            // завершаем обработку, иначе уже разрушенный контекст
-                            // будет передан панелям
-                            return;
-
+                            // если сейчас основной режим
+                            if (currentMode.equals(Mode.WORK)) {
+                                // закрываем окно
+                                window.close();
+                                // завершаем обработку, иначе уже разрушенный контекст
+                                // будет передан панелям
+                                return;
+                            } else if (currentMode.equals(Mode.INFO)) {
+                                currentMode = Mode.WORK;
+                            }
                         }
                         case TAB -> InputFactory.nextTab();
                         case W -> window.close();
@@ -208,9 +240,16 @@ public static final int C_RAD_IN_PX = 4;
                     }
             }
         }
-        panelControl.accept(e);
-        panelRendering.accept(e);
-        panelLog.accept(e);
+        switch (currentMode) {
+            case INFO -> panelInfo.accept(e);
+            case FILE -> {}
+            case WORK -> {
+                // передаём события на обработку панелям
+                panelControl.accept(e);
+                panelRendering.accept(e);
+                panelLog.accept(e);
+            }
+        }
     }
     /**
      * Рисование
@@ -228,6 +267,11 @@ public static final int C_RAD_IN_PX = 4;
         panelControl.paint(canvas, windowCS);
         panelLog.paint(canvas, windowCS);
         panelHelp.paint(canvas, windowCS);
+        // рисуем диалоги
+        switch (currentMode) {
+            case INFO -> panelInfo.paint(canvas, windowCS);
+            case FILE -> {}
+        }
         canvas.restore();
     }
 }
